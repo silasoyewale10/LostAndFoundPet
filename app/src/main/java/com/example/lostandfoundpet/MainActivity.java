ad524fb9a71844b3e5b7d9abb627372ea93eab77
petewhartnett <petewhartnett@outlook.com>
2020-03-10 11:50:26 -0700

    location set up

--- app/src/main/java/com/example/lostandfoundpet/MainActivity.java
+++ app/src/main/java/com/example/lostandfoundpet/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.lostandfoundpet;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class MainActivity extends AppCompatActivity {
+
+    //for location services
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        boolean permissionToAccessCoarseLocation = ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+                   == PackageManager.PERMISSION_GRANTED;
+        if(!permissionToAccessCoarseLocation){
+            ActivityCompat.requestPermissions(this, new String[]{
+
+                Manifest.permission.ACCESS_COARSE_LOCATION}, 73);
+            }
+        else{
+            getUserLocation();
+        }
+
 
 
 
@@ -58,5 +85,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void getUserLocation() {
+
+
+
+        fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                            }
+                        }
+                    });
+
+
+
+    }
+
 }
 
