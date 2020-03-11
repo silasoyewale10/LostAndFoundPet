53830db0d359938ffc16082598b6d124d4527675
holliemaethomas <holliemaethomas85@gmail.com>
2020-03-11 09:18:59 -0700

    get pic from emulator method and updated found pet xml

--- app/src/main/java/com/example/lostandfoundpet/FoundPet.java
+++ app/src/main/java/com/example/lostandfoundpet/FoundPet.java
@@ -2,13 +2,66 @@ package com.example.lostandfoundpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;import java.io.FileNotFoundException;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class FoundPet extends AppCompatActivity {
 
+
+    TextView textTargetUri;
+    ImageView targetImage;
+
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found_pet);
+        final Button buttonLoadImage = (Button)findViewById(R.id.loadimage);
+        textTargetUri = (TextView)findViewById(R.id.targeturi);
+        targetImage = (ImageView)findViewById(R.id.targetimage);
+
+        buttonLoadImage.setOnClickListener(new Button.OnClickListener(){
+
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+                Intent intent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 0);
+                buttonLoadImage.setText("Add Details");
+
+            }});
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+            textTargetUri.setText(targetUri.toString());
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+                targetImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 }
